ae15ec0b4c32eac9018f22a807c149ba48777361
Mikulew <mikulew@gmail.com>
2021-09-02 11:48:26 +0200

    Program which check from two giver integers whether ine is positive and another one is negative

---
+++ exercises/008.js
@@ -0,0 +1,11 @@
+/*
+  Write a JavaScript program to check from two given integers, whether one is positive and another one is negative.
+*/
+
+function checkIsPositiveNegative(x, y) {
+  return ((x < 0 && y > 0) || (x > 0 && y < 0));
+}
+console.log(checkIsPositiveNegative(2, 2));
+console.log(checkIsPositiveNegative(-2, 2));
+console.log(checkIsPositiveNegative(2, -2));
+console.log(checkIsPositiveNegative(-2, -2));
