9cb981e9ff37e368eee87dc23e293dc92ec1dc29
Mikulew <mikulew@gmail.com>
2021-09-10 10:39:35 +0200

    Find all 13th Friday days of a given year or range

---
+++ exercises/013.js
@@ -0,0 +1,24 @@
+/*
+  Write a JavaScript program to find all 13th Friday days of a given year or range.
+*/
+
+const FRIDAY = 4;
+
+function numberOfFridayThe13thsIn(startYear, endYear) {
+  let counter = 0;
+  endYear ??= startYear;
+  for (let year = startYear; year <= endYear; year++) {
+    for (let month = 0; month <= 11; month++) {
+      const date = new Date(year, month, 13);
+      if (date.getDay() === 5) {
+        counter++;
+      }
+    }
+  }
+  return startYear === endYear
+    ? `Number of 13th Friday in ${startYear} is ${counter}`
+    : `Number of 13th Friday between ${startYear} and ${endYear} is ${counter}`;
+}
+
+console.log(numberOfFridayThe13thsIn(2012, 2012)); // Number of 13th Friday in 2012 is 3
+console.log(numberOfFridayThe13thsIn(2012, 2015)); // Number of 13th Friday between 2012 and 2015 is 9
